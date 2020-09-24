19bc2bc3394edb725c5541255c2b7389ccf5ad17
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2020-09-24 13:00:33 +0200

    Added mik.php
    min initial fil til opgaven

---
+++ mik.php
@@ -0,0 +1,18 @@
+<?php
+// Min første php fil til opgaven
+//print Hello World
+$output = 'Hello World';
+//echo 'Hello world';
+
+
+// Diverse variabler i php
+// hvis man f.eks skal bruge ' eller " som en del af sin tekst
+// skal man lave en /' eller /". Det vil forhindre typiske fejl.
+$number = 4;
+$float = 4.4;
+$bool1 = true;
+$string1 = 'Hello';
+$string2 = 'World';
+// Concatination af strings
+echo $string1 , ' ', $string2;
+?>
